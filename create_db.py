f9c68f65d1179d94fc31a9d974837b522ec1741b
Carlos Mina Lopez <cminalopez@outlook.com>
2024-03-02 23:34:09 -0500

    Relevancy score is low lol won't give a good output.

--- create_db.py
+++ create_db.py
@@ -2,45 +2,53 @@ from langchain_community.document_loaders import DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.vectorstores.chroma import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.schema import Document
 import os
 import shutil
 
-#path to the data
+# path to the data
 DATA_PATH = 'data'
 CHROMA_PATH = 'chroma'
-EMBEDDING_MODEL = "intfloat/e5-large-v2"
 
 def main():
     documents = load_docs()
     chunks = split_pages(documents)
     save_to_db(chunks)
 
-#load the .txt files
+
+# load the .txt files
 def load_docs():
     loader = DirectoryLoader(DATA_PATH)
     documents = loader.load()
-    doc_text = [doc.page_content for doc in documents]
-    return doc_text
+    return documents
 
-#split the documents into chunks of text
-def split_pages(doc_text):
+
+# split the documents into chunks of text
+def split_pages(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=200, chunk_overlap=25, length_function=len,
+        chunk_size=300,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
     )
-    chunks = text_splitter.create_documents(doc_text)
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
     return chunks
 
-#save files to database
+
+# save files to database
 def save_to_db(chunks):
-    #clear db if it exists
+    # clear db if it exists
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
-    #create new db from current docs
-    db= Chroma.from_documents(
+    # create new db from current docs
+    db = Chroma.from_documents(
         chunks, HuggingFaceEmbeddings(), persist_directory=CHROMA_PATH
     )
-    #print(chunks[10:13])
+    # print(chunks[10:13])
     db.persist()
+
 
 if __name__ == '__main__':
     main()
